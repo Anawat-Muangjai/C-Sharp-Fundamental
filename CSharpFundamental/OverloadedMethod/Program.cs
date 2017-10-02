619c2c3d8750596ed46464ca4a0d151490613cd4
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-02 10:42:56 +0700

    Add Overloaded Method example

---
+++ CSharpFundamental/OverloadedMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OverloadedMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Foo(99);
+            Foo("text");
+            Console.ReadKey();
+        }
+
+        static void Foo(int x)
+        {
+            Console.WriteLine("Foo(int x)");
+        }
+
+        static void Foo(string y)
+        {
+            Console.WriteLine("Foo(String y)");
+        }
+    }
+}
