6a70b071c3ddef7de3bce3ac0baa298f934ff675
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-02 11:44:37 +0700

    Add Enums example

---
+++ CSharpFundamental/Enums/EnumDayMonth.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Enums
+{
+    enum Days
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    };
+
+    enum Month : byte
+    {
+        Jan,
+        Feb,
+        Mar,
+        Apr,
+        May,
+        Jun,
+        July,
+        Aug,
+        Sep,
+        Oct,
+        Nov,
+        Dec
+    };
+}
---
+++ CSharpFundamental/Enums/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Days today = Days.Sunday;
+            int dayNumber = (int)today;
+
+            Console.WriteLine("{0} is day number # {1}.", today, dayNumber);
+
+            Month thisMonth = Month.Jan;
+            byte monthNumber = (byte)thisMonth;
+            Console.WriteLine("{0} is month number # {1}.", thisMonth, monthNumber);
+            Console.ReadKey();
+        }
+    }
+}
