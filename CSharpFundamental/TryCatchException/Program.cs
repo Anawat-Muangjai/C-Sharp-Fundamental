d74abd2748fd01e094c1f324c6f56e261f4e7cda
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-02 10:57:10 +0700

    Add Try Catch Exception example

---
+++ CSharpFundamental/TryCatchException/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TryCatchException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string s = null;
+                ProcessString(s);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("{0} First Exception caught.", e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Second Exception caught", e);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        static void ProcessString(string s)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException();
+            }
+        }
+    }
+}
