5ace21fec98da22fa93945fc3b7c907fbebb7afd
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-02 10:46:21 +0700

    Add Method Optional Parameters example

---
+++ CSharpFundamental/MethodOptionalParameters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MethodOptionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method();
+            Method(4);
+            Method(4, "Dog");
+            Method(name: "Brid");
+            Console.ReadKey();
+        }
+
+        static void Method(int value = 1, string name = "Peal")
+        {
+            Console.WriteLine("Value = {0} , Name = {1} ", value, name);
+        }
+    }
+}
