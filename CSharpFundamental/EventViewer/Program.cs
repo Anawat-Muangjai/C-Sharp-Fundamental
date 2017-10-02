8cf06a145708a1edf2b34edf9bcb5d7b7cb2fa28
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-02 11:51:48 +0700

    Add Event Viewer example

---
+++ CSharpFundamental/EventViewer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace EventViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eventLog = "Application";
+            string eventSource = "Logging Demo App";
+            string eventMessage = "Hello from Logging Demo App";
+
+            //Create the event source if it doesn't already exits
+            if (!EventLog.SourceExists(eventSource))
+                EventLog.CreateEventSource(eventSource, eventLog);
+
+            //Log the message
+            EventLog.WriteEntry(eventSource, eventMessage, EventLogEntryType.Warning, 234);
+            Console.ReadKey();
+        }
+    }
+}
