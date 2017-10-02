9a867fe7b7b0ad21691acbc05536d583bda278d4
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-02 10:35:53 +0700

    Add TwoDimensionArray example

---
+++ CSharpFundamental/TwoDimensionArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TwoDimensionArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] array = new string[,]
+{
+                {"cat","dog"},
+                {"bird","fish"}
+};
+            Console.WriteLine(array[0, 0]);
+            Console.WriteLine(array[0, 1]);
+            Console.WriteLine(array[1, 0]);
+            Console.WriteLine(array[1, 1]);
+            Console.ReadKey();
+        }
+    }
+}
