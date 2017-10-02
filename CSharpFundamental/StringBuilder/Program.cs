90104f227b06c76788304221fec0706113d0d390
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-02 10:28:13 +0700

    Add StringBuilder example

---
+++ CSharpFundamental/StringBuilder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringBuilderEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare a new StringBulider
+            StringBuilder builder = new StringBuilder();
+            builder.Append("The list starts here:");
+            builder.AppendLine();
+            builder.Append("1 cat").AppendLine();
+            builder.Append("2 dog").AppendLine();
+            builder.Append("3 bird").AppendLine();
+
+            //Get a reference to the stringBuilder's buffer content.
+            string innerString = builder.ToString();
+
+            //Display
+            Console.WriteLine(innerString);
+            Console.ReadKey();
+        }
+    }
+}
