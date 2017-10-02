9bfbe0cc3a500ff8a192c3e434412d94d1ccc11b
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-02 11:38:07 +0700

    Add Ternary Operator example

---
+++ CSharpFundamental/TernaryOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TernaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetValue("Sam"));
+            Console.WriteLine(GetValue("Jane"));
+            Console.ReadKey();
+        }
+
+        static int GetValue(string name)
+        {
+            //Replace if by ? (TernaryOperator)
+            return name == "Sam" ? 100 : -1;
+        }
+    }
+}
