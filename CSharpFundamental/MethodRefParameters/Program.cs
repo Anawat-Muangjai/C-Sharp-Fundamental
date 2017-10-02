300b293c12db3895ab6604084537df4fd6a09ea1
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-02 10:53:15 +0700

    Add Method Ref Parameters example

---
+++ CSharpFundamental/MethodRefParameters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MethodRefParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = 1;
+            Method(ref val);
+            Console.WriteLine(val);
+            Console.ReadKey();
+        }
+
+        static void Method(ref int i)
+        {
+            i = i + 44;
+        }
+    }
+}
