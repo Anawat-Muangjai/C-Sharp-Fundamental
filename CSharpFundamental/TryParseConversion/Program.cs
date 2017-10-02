ba7c12371a955525c0e1a34f8516925f159977a1
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-02 10:20:31 +0700

    Add TryParse conversion example

---
+++ CSharpFundamental/TryParseConversion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TryParseConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            string numberString = "1234";
+            if (int.TryParse(numberString, out number))
+            {
+                Console.WriteLine("Convertion successed, number now equal " + number);
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed, number now equal " + number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
