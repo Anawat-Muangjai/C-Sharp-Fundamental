8e85ebf9f654c644cde3850bf2e4ee8d50691997
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-02 10:32:31 +0700

    Add RegularExpression example

---
+++ CSharpFundamental/RegularExpression/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegularExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var textToTest = "anawat.mu@outlook.com";
+            var regularExpression = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
+            var result = Regex.IsMatch(textToTest, regularExpression, RegexOptions.None);
+            if (result)
+            {
+                Console.WriteLine("Text matched expression");
+            }
+            Console.ReadKey();
+        }
+    }
+}
